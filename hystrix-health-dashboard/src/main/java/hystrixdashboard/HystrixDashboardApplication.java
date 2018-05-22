16cbf5ff5c1c104adac88c9651dfa60856456c76
Vadim Shavlovski <vadimshavlovski@mail.ru>
2018-05-22 22:34:18 +0300

    added API Gateway and made some refactor

--- hystrix-health-dashboard/src/main/java/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-health-dashboard/src/main/java/hystrixdashboard/HystrixDashboardApplication.java
@@ -3,11 +3,15 @@ package hystrixdashboard;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableHystrixDashboard
+@Controller
 public class HystrixDashboardApplication  extends SpringBootServletInitializer {
 
 	@RequestMapping("/")
