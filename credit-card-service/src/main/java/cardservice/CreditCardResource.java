ea173236ef9dd6d986456697c4de0e271aa371fb
Vadim Shavlovski <vadimshavlovski@mail.ru>
2018-05-26 21:08:49 +0300

    added config server

--- credit-card-service/src/main/java/cardservice/CreditCardResource.java
+++ credit-card-service/src/main/java/cardservice/CreditCardResource.java
@@ -1,14 +1,18 @@
 package cardservice;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RefreshScope
 public class CreditCardResource {
+
+    @Value("${profile}")
+    private String profile;
 
     @Autowired
     private CreditCardService creditCardService;
@@ -17,5 +21,12 @@ public class CreditCardResource {
     public Integer addCardNumber(@PathVariable String cardNumber) {
         return creditCardService.addCardNumber(cardNumber);
     }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return this.profile;
+    }
+
+
 
 }
